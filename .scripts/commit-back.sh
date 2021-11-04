b16233899d2ff02691e447759acfec9e02affec4
Adauto VJR <adautovjr@yahoo.com>
2021-11-04 10:10:08 -0300

    Added commit-back script

--- .scripts/commit-back.sh
+++ .scripts/commit-back.sh
@@ -13,6 +13,6 @@ st
 set CURRENT_BRANCH (git rev-parse --abbrev-ref HEAD)
 cm $argv[1]
 push --set-upstream origin $CURRENT_BRANCH
-set PR_LINK (gh pr create --title "$argv[2]" --body "" -B "stag/front")
+set PR_LINK (gh pr create --title "$argv[2]" --body "" -B "stag/backend")
 echo "[PR Open] <$PR_LINK|$argv[2]>"
 #slack --text "[PR Open] <$PR_LINK|$argv[2]>" --channel '#folio-all-devs'
--- .scripts/mastersetup.sh
+++ .scripts/mastersetup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+cd
+
 export FISH="$HOME/.config/fish/config.fish"
+
+curl -O https://raw.githubusercontent.com/rockymadden/slack-cli/master/src/slack
+chmod +x slack
 
 git clone --bare git@github.com:adautovjr/.dotfiles.git $HOME/.dotfiles
 git --git-dir=$DOTFILES_DIR --work-tree=$HOME checkout
