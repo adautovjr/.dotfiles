5db89701bb9a632971769de98455d72d96010f0e
Adauto Vieira Jr <adautovjr@yahoo.com>
2022-04-11 23:19:45 -0300

    Update mastersetup.sh

--- .scripts/mastersetup.sh
+++ .scripts/mastersetup.sh
@@ -8,6 +8,6 @@ curl -O https://raw.githubusercontent.com/rockymadden/slack-cli/master/src/slack
 chmod +x slack
 
 git clone --bare git@github.com:adautovjr/.dotfiles.git $HOME/.dotfiles
-git --git-dir=$DOTFILES_DIR --work-tree=$HOME checkout
-git --git-dir=$DOTFILES_DIR --work-tree=$HOME config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
 
