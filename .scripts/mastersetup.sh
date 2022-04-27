6103eed8ab7a387f61a0dece86d2738cc139a7b2
Adauto Guedes <adautoguedes@AJs-MacBook-Pro.local>
2022-04-27 11:24:41 -0300

    fix: Moved Brewfile to a different folder

--- .scripts/mastersetup.sh
+++ .scripts/mastersetup.sh
@@ -4,10 +4,15 @@ cd
 
 export FISH="$HOME/.config/fish/config.fish"
 
-curl -O https://raw.githubusercontent.com/rockymadden/slack-cli/master/src/slack
-chmod +x slack
-
 git clone --bare git@github.com:adautovjr/.dotfiles.git $HOME/.dotfiles
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
 
+xcode-select --install
+
+curl -O https://raw.githubusercontent.com/rockymadden/slack-cli/master/src/slack
+chmod +x slack
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew bundle -file $HOME/.dotfiles/Brewfile
