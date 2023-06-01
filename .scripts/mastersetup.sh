37caafcf904a0710fdcc4cdcba3307db066e7377
Adauto Vieira Jr <26850257+adautovjr@users.noreply.github.com>
2023-05-31 23:26:24 -0300

    Update mastersetup.sh

--- .scripts/mastersetup.sh
+++ .scripts/mastersetup.sh
@@ -6,12 +6,10 @@ git clone --bare git@github.com:adautovjr/.dotfiles.git $HOME/.dotfiles
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install bundle
-
-brew bundle --file $HOME/Brewfile
+brew bundle install
 
 # xcode-select --install
