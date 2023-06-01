841bd5d721773fe740b0bea27369cf350b8a65a7
Adauto Vieira Jr <26850257+adautovjr@users.noreply.github.com>
2023-05-31 23:18:43 -0300

    Update mastersetup.sh

--- .scripts/mastersetup.sh
+++ .scripts/mastersetup.sh
@@ -6,9 +6,6 @@ git clone --bare git@github.com:adautovjr/.dotfiles.git $HOME/.dotfiles
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
 
-curl -O https://raw.githubusercontent.com/rockymadden/slack-cli/master/src/slack
-chmod +x slack
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
