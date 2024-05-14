ab29117f7489f6f5a6ffbba488ffb62ff3fd4234
Adauto Guedes <adautoguedes@Adauto-Guedes-MacBook-Pro-ArcTouch-546A.local>
2024-05-14 11:16:20 -0300

    chore: Update backup

--- .profile
+++ .profile
@@ -1,3 +1,4 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 source $HOME/.alias
+source $HOME/.bashrc
 # source $HOME/.env
--- .scripts/mastersetup.sh
+++ .scripts/mastersetup.sh
@@ -6,6 +6,8 @@ git clone --bare git@github.com:adautovjr/.dotfiles.git $HOME/.dotfiles
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
 
+defaults write -g ApplePressAndHoldEnabled -bool true
+
 sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
